163a03d93f8a05081cddbe9edbb8932a8292159f
Sascha Hanssen <hanssen@meso.net>
2010-03-12 14:44:41 +0100

    - fix: oca_mailer initializer was broken

--- config/initializers/oca_mailer.rb
+++ config/initializers/oca_mailer.rb
@@ -3,10 +3,9 @@
 if RAILS_ENV != 'test'
   oca_mailer_config_file = "#{RAILS_ROOT}/config/oca_mailer.yml"
   if File.exist? oca_mailer_config_file
+    email_settings = YAML::load(File.open(oca_mailer_config_file))
     unless email_settings[RAILS_ENV].nil?
       ActionMailer::Base.delivery_method = :smtp
-
-      email_settings = YAML::load(File.open(oca_mailer_config_file))
       ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV]
     end
   end

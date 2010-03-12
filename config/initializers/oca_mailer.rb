4d8d3692ff364d78cdcb2cf70e775a16e15710f7
Sascha Hanssen <hanssen@meso.net>
2010-03-12 14:40:39 +0100

    - using system mailer if oca_mailer.yml does not exist or nothing defines for current environment

--- config/initializers/oca_mailer.rb
+++ config/initializers/oca_mailer.rb
@@ -1,9 +1,14 @@
 
 # Load mail configuration if not in test environment
 if RAILS_ENV != 'test'
-  ActionMailer::Base.delivery_method = :smtp
+  oca_mailer_config_file = "#{RAILS_ROOT}/config/oca_mailer.yml"
+  if File.exist? oca_mailer_config_file
+    unless email_settings[RAILS_ENV].nil?
+      ActionMailer::Base.delivery_method = :smtp
 
-  email_settings = YAML::load(File.open("#{RAILS_ROOT}/config/oca_mailer.yml"))
-  ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV] unless email_settings[RAILS_ENV].nil?
+      email_settings = YAML::load(File.open(oca_mailer_config_file))
+      ActionMailer::Base.smtp_settings = email_settings[RAILS_ENV]
+    end
+  end
 end
 
